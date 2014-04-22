9508087763c8c33a244d0f82daa67d2a15e4dd9f
Anoop Thomas Mathew <atmb4u@gmail.com>
2014-04-22 17:29:51 +0530

    version bumped to v0.986

--- marlin/marlin.py
+++ marlin/marlin.py
@@ -11,7 +11,7 @@ from redis import Redis, ConnectionError
 
 from flask import make_response, request, current_app, Response, Flask, render_template, url_for
 
-VERSION = "0.985"
+VERSION = "0.986"
 
 config = ConfigParser.ConfigParser()
 config.read("marlin.config")
